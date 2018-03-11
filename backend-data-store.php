cac444686bc4f55265c92b0cc51f63bbe9cb3692
Sajid Rahman <sajidrahman25@gmail.com>
2018-03-11 16:58:20 -0400

    write individual data

--- backend-data-store.php
+++ backend-data-store.php
@@ -36,8 +36,9 @@ $json_str = file_get_contents('php://input');
 
 # Get as an object
 $json_obj = json_decode($json_str);
+$filename = $json_obj[0]->id.'.json';
 //write json to file
-if (file_put_contents($json_obj[0]->id.'.json', $json))
+if (file_put_contents($filename, $json_str))
   echo "JSON file created successfully...";
 else
   echo "Oops! Error creating json file...";
